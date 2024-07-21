7371ec5f635b48fcc8686aea50155f3dc7abbbc9
Lyndon <xianzhiwu@trip.com>
2024-07-21 18:29:11 +0800

    feat: list & create & help

--- bin/lyndon.js
+++ bin/lyndon.js
@@ -1,35 +1,18 @@
 #!/usr/bin/env node
-import { clone } from "../lib/clone.js";
-import chalk from "chalk";
-import figlet from "figlet";
 import { program, Option } from "commander";
-import { templates } from "../lib/constants.js";
 import fs from "fs-extra";
+import { create } from "../lib/create.js";
+import { list } from "../lib/list.js";
+import { help } from "../lib/help.js";
 
 const pkg = fs.readJSONSync(new URL("../package.json", import.meta.url));
-
 program.version(pkg.version, "-v, --version", "output the current version");
 
 program
   .name("lyndon")
   .description("lyndon is a cli tool for engineering")
   .usage("<command> [options]")
-  .on("--help", () => {
-    console.log(
-      `\r\n ${chalk.yellow(
-        figlet.textSync("lyndon-cli", {
-          font: "standard",
-          horizontalLayout: "default",
-          verticalLayout: "default",
-          width: 120,
-          whitespaceBreak: true,
-        })
-      )}`
-    );
-    console.log(
-      `Run ${chalk.cyan(`lyndon <command> --help`)} for more usaged.`
-    );
-  })
+  .on("--help", help)
   .addOption(new Option("-h, --help", "show the help information"));
 
 program
@@ -38,25 +21,8 @@ program
   .option("-t, --template [template]", "template name")
   .option("-f, --force", "overwrite target directory if it exists")
   .option("-y, --yes", "do not prompt for confirmation")
-  .action(async (name, options) => {
-    console.log(`create ${name}`);
-    console.log(options);
-  });
+  .action((name, options) => create(name, options));
 
-program
-  .command("list")
-  .description("list all templates")
-  .action(() => {
-    console.log(chalk.yellowBright("templates:"));
-    templates.forEach((item, idx) => {
-      console.log(
-        `${idx + 1}.${chalk.greenBright(item.name)} -> ${chalk.greenBright(
-          item.desc
-        )}`
-      );
-    });
-  });
-
-// await clone("yingside/vite-template", "test-temp");
+program.command("list").description("list all templates").action(list);
 
 program.parse(process.argv);
